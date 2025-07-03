201f4d8668b7c36e1fccc67706e699dddcdeb42b
get-co <virat983816@gmail.com>
2025-07-03 20:21:32 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,5 @@
 window.onload = function() {
-    setTimeout(function() {
-    location.reload();
-  }, 120000); // Wait for 1 second before executing the code
+    
   if (window.innerWidth < 499 ) {
   // Get the current date and time
 function getNum() {
